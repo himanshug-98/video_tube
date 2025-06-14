f5114ff589c4997d1e3fdefccf81f84416b5baf0
himanshug-98 <himanshu_9970@yahoo.com>
2025-06-15 03:48:04 +0530

    creating navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./Home.css'
+import { Sidebar } from '../../components/Sidebar/Sidebar'
+const Home = () => {
+  return (
+    <>
+       <Sidebar/>
+    </>
+  )
+}
+
+export default Home
